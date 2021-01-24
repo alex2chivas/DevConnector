4f8d7abb20eb205a1e31d493d21e612363221550
Alexis Flores <alex2chivas>
2021-01-24 11:54:13 -0800

    ''

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,8 @@ import Spinner from '../layout/Spinner'
 import { getProfileById } from '../../actions'
 import ProfileTop from './PorfileTop'
 import ProfileAbout from './PorfileAbout'
+import ProfileExperience from './ProfileExperience'
+import ProfileEducation from './ProfileEducation'
 
 const Profile = ({
   getProfileById,
@@ -38,6 +40,37 @@ const Profile = ({
           <div class='profile-grid my-1'>
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
+            <div className='profile-exp bg-white p-2'>
+              <h2 className='text-primary'>Experience</h2>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.experience.map(experience => (
+                    <ProfileExperience
+                      key={experience._id}
+                      experience={experience}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4> No experience credentials</h4>
+              )}
+            </div>
+
+            <div className='profile-edu bg-white p-2'>
+              <h2 className='text-primary'>Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map(education => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4> No Education credentials</h4>
+              )}
+            </div>
           </div>
         </Fragment>
       )}
