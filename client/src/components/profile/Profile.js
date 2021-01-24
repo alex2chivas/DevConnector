d0e0666158c8cca77f12f89a1de5f48438e5a8b0
Alexis Flores <alex2chivas>
2021-01-23 16:39:05 -0800

    added inividual profiles when clicked

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,23 +1,44 @@
 import React, { Fragment, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 import Spinner from '../layout/Spinner'
 import { getProfileById } from '../../actions'
+import ProfileTop from './PorfileTop'
 
 const Profile = ({
   getProfileById,
-  profile: { profile, loading, auth },
+  profile: { profile, loading },
+  auth,
   match
 }) => {
-  console.log(match.params.id)
   useEffect(() => {
     getProfileById(match.params.id)
-  }, [getProfileById])
+  }, [getProfileById, match.params.id])
 
   return (
     <Fragment>
-      <div>Profile</div>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Fragment>Profile</Fragment>
+          <Link to='/profiles' className='btn btn-light'>
+            Back To Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            !auth.loading &&
+            auth.user._id === profile.user._id && (
+              <Link to='/edit-profile' className='btn btn-dark'>
+                Edit Profile
+              </Link>
+            )}
+          <div class='profile-grid my-1'>
+            <ProfileTop profile={profile} />
+          </div>
+        </Fragment>
+      )}
     </Fragment>
   )
 }
